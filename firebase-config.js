209cf21cb3ab6145992d2716fab70ceccc541046
Adarshkumarsingh1212 <adarshkumarsinghmac@gmail.com>
2024-11-23 23:15:49 +0530

    Update and rename index2.html to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDf8fmHUMxumxnCF4AHyvNaEZUi0d_kT3U",
+  authDomain: "adarshk627fag628.firebaseapp.com",
+  projectId: "adarshk627fag628",
+  storageBucket: "adarshk627fag628.firebasestorage.app",
+  messagingSenderId: "757467919089",
+  appId: "1:757467919089:web:53274bd6346ca4c858ef50",
+  measurementId: "G-VCXEC0DBB1"
+};
+
+//initialize Firebase
+firebase.initializeApp(firebaseCongig);
